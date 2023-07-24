51c131ee225ea22165205c71ece2a2ecf56bc3c3
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-24 16:30:34 -0700

    nav logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from "react";
 import "@styles/globals.css";
 import { Metadata } from "next";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata: Metadata = {
   title: "Artifcial Prompts",
@@ -18,7 +20,10 @@ const RootLayout = ({
         <div className='main'>
           <div className='gradient' />
         </div>
-        <main className='app'>{children}</main>
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
 
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  signIn,
+  signOut,
+  useSession,
+  getProviders,
+} from "next-auth/react";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link className='flex gap-2 flex-center' href={"/"}>
+        <Image
+          src={"/assets/images/logo.svg"}
+          width={30}
+          height={30}
+          alt='logo'
+          className='object-contain'
+        />
+        <p className='logo_text'>Promptopia</p>
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
