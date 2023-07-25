61d23518c0afe11d7fe721c75cdb160073ea5d03
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-25 15:47:43 -0700

    SessionProvider

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Provider = () => {
-  return <div>Provider</div>;
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+
+const Provider = ({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session: Session | null | undefined;
+}) => {
+  return (
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
+  );
 };
 
 export default Provider;
