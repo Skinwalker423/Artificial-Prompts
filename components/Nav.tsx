b278a848ac55e4fdab530ad93f1cf1a437060dfb
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-24 20:38:48 -0700

    sign in button and avatar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -10,6 +10,7 @@ import {
   getProviders,
 } from "next-auth/react";
 const Nav = () => {
+  const isLoggedIn = true;
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link className='flex gap-2 flex-center' href={"/"}>
@@ -22,6 +23,37 @@ const Nav = () => {
         />
         <p className='logo_text'>Promptopia</p>
       </Link>
+      {/* mobile navigation */}
+      <div className='sm:flex-hidden'>
+        {isLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            {" "}
+            <Link
+              className='black_btn'
+              href={"/create-prompt"}
+            >
+              Create Post
+            </Link>
+            <button
+              onClick={() => signOut()}
+              className='outline_btn'
+            >
+              Sign Out
+            </button>
+            <Link href={"/profile"}>
+              <Image
+                src={"/assets/images/logo.svg"}
+                width={37}
+                height={37}
+                alt='avatar image'
+                className='rounded-full'
+              />
+            </Link>
+          </div>
+        ) : (
+          <button className='outline_btn'>Sign in</button>
+        )}
+      </div>
     </nav>
   );
 };
