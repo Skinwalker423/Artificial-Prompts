34b31705d60ddb190377a2b4c42e2b9456386613
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-27 16:20:01 -0700

    FormProps

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
-import { UseSessionOptions } from "next-auth/react";
+import React, { FormEvent, useState } from "react";
+import {
+  UseSessionOptions,
+  useSession,
+} from "next-auth/react";
 import { useRouter } from "next/router";
 import Form from "@components/Form";
 
@@ -9,7 +12,22 @@ const CreatePrompt = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ post: "", tag: "" });
 
-  return <Form />;
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const createPrompt = async (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Form
+      type={"Create"}
+      post={post}
+      setPost={setPost}
+      isSubmitting={isSubmitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, {
+  Dispatch,
+  EventHandler,
+  FormEventHandler,
+  SetStateAction,
+} from "react";
 
-function Form() {
+interface FormProps {
+  type: string;
+  post: { post: string; tag: string };
+  setPost: Dispatch<
+    SetStateAction<{ post: string; tag: string }>
+  >;
+  isSubmitting: boolean;
+  handleSubmit: FormEventHandler<HTMLInputElement>;
+}
+
+function Form({
+  type,
+  post,
+  setPost,
+  isSubmitting,
+  handleSubmit,
+}: FormProps) {
   return <div>Form</div>;
 }
 
