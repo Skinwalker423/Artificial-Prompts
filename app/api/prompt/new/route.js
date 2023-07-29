406a51deef5d45c1e9745877be0663d02b576660
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-28 21:13:57 -0700

    ceate new Prompt api

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,10 +1,29 @@
 import { NextResponse } from "next/server";
+import Prompt from "@models/prompt";
+import { connectToMongoDb } from "@utils/database";
+import mongoose from "mongoose";
 
-export async function POST(req, res) {
-  const body = await req.json();
-  console.log("body in api", body);
+export async function POST(req) {
+  const { userId, prompt, tag } = await req.json();
+  console.log("body in api", userId, prompt, tag);
 
-  return NextResponse.json({ data: "data" });
+  try {
+    await connectToMongoDb();
+    const newPrompt = new Prompt({
+      userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+    return new NextResponse(JSON.stringify(newPrompt), {
+      status: 201,
+    });
+  } catch (error) {
+    return new NextResponse(error, {
+      status: 500,
+    });
+  }
 }
 
 export async function GET(req, res) {
