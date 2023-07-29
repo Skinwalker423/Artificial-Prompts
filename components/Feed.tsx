ac5fb631ad62ca0468533d8e11873e5fd4c288a5
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-28 21:40:57 -0700

    Feed form

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+
+import React, { FormEventHandler, useState } from "react";
+import PromtCard from "./PromtCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (
+    e
+  ) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className='feed'>
+      <form
+        onSubmit={handleSubmit}
+        className='relative w-full flex-center'
+      >
+        <input
+          type='text'
+          placeholder='search for a tag or username'
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Feed;
