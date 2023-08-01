576feee1086b72a74fc51d03dd5eb113ab2c55f2
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-01 14:55:59 -0700

    redirecting to update-prompt

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { ProfileProps, PromptProps } from "@types";
 import PromtCard from "./PromtCard";
+import { useRouter } from "next/navigation";
 
 const Profile = ({ name, desc, data }: ProfileProps) => {
   console.log("data from api", data);
+  const router = useRouter();
 
-  const handleEdit = (post: PromptProps) => {};
+  const handleEdit = (post: PromptProps) => {
+    console.log("redirecting to edit");
+    router.push(`/update-prompt?id=${post._id}`);
+  };
   const handleDelete = async (post: PromptProps) => {};
 
   return (
